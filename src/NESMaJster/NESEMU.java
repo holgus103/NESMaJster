eb68333c8792b226d72b426d90e3f19a77fe7fbb
Łukasz Ławniczak <lawniczakl@student.mini.pw.edu.pl>
2015-07-15 18:53:57 +0200

    - wyświetlanie tego co stworzyło PPU w okienku (jeszcze trzeba będzie to przerobić, ale coś działa) - prawidłowo wyświetla się przykład testowy - PPU do testowania (i poprawiania pewnie)

--- src/NESMaJster/NESEMU.java
+++ src/NESMaJster/NESEMU.java
@@ -5,6 +5,10 @@
  */
 package NESMaJster;
 
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
 /**
  *
  * @author Administrator
@@ -14,9 +18,61 @@ public class NESEMU {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-            System.out.printf("Hello user!");
-    CPU asd = new CPU();
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame=new JFrame("Demo");
+        PPU ppu=new PPU();
+        CPU cpu=new CPU();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BufferedImage bi=new BufferedImage(256,240, BufferedImage.TYPE_INT_RGB);
+        Random r = new Random();
+        for(int i=0;i<256;++i)
+            for(int j=0;j<240;++j)
+                bi.setRGB(i,j, r.nextInt());
+        ppu.ZeroVRAM();
+        //podkreslenie gornej lini (tlo)
+        for(int j=0;j<2;++j)
+            ppu.VRAM.setByte((short)(j*8),(byte)0xFF);
+        //literka T
+        for(int j=0;j<2;++j) {
+            for (int i = 0; i < 3; ++i)
+                ppu.VRAM.setByte((short) (16+i+j*8), (byte) 0xFF);
+            for (int i = 3; i < 8; ++i)
+                ppu.VRAM.setByte((short) (16+i+j*8), (byte) 0x18);
+        }
+        //literka E
+        for(int j=0;j<2;++j) {
+            ppu.VRAM.setByte((short)(32+8*j), (byte) 0xFF);
+            ppu.VRAM.setByte((short)(39+8*j),(byte)0xFF);
+            for(int i=1;i<3;++i)
+                ppu.VRAM.setByte((short)(32+i+j*8), (byte) 0x03);
+            for(int i=3;i<5;++i)
+                ppu.VRAM.setByte((short)(32+i+j*8), (byte) 0x1F);
+            for(int i=5;i<7;++i)
+                ppu.VRAM.setByte((short)(32+i+j*8), (byte) 0x03);
+        }
+        //literka S
+        for(int j=0;j<2;++j) {
+            for(int i=0;i<2;++i)
+                ppu.VRAM.setByte((short)(48+i+j*8), (byte) 0xFF);
+            for(int i=2;i<3;++i)
+                ppu.VRAM.setByte((short)(48+i+j*8), (byte) 0x03);
+            for(int i=3;i<5;++i)
+                ppu.VRAM.setByte((short)(48+i+j*8), (byte) 0xFF);
+            for(int i=5;i<6;++i)
+                ppu.VRAM.setByte((short)(48+i+j*8), (byte) 0xC0);
+            for(int i=6;i<8;++i)
+                ppu.VRAM.setByte((short)(48+i+j*8), (byte) 0xFF);
+        }
+        ppu.putSprite(0, (byte) 30, (byte) 126, (byte) 0,(byte)1);
+        ppu.putSprite(1,(byte)40,(byte)126,(byte)0,(byte)2);
+        ppu.putSprite(2,(byte)50,(byte)126,(byte)0,(byte)3);
+        ppu.putSprite(3,(byte)60,(byte)126,(byte)0,(byte)1);
+        ppu.VRAM.setByte((short) 0x3F03, (byte) 1);
+        ppu.VRAM.setByte((short)0x3F13,(byte)1);
+        System.out.printf("%x",ppu.VRAM.readByte((short)6));
+        frame.getContentPane().add(new JLabel(new ImageIcon(ppu.draw())));
+        frame.pack();
+        frame.setVisible(true);
 // TODO code application logic here
     }
     
--- src/NESMaJster/PPU.java
+++ src/NESMaJster/PPU.java
@@ -7,6 +7,7 @@ package NESMaJster;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.awt.image.BufferedImage;
 import java.util.List;
 
 /**
@@ -26,11 +27,14 @@ public class PPU {
     byte PPUSTATUS,OAMADDR,OAMDATA,PPUSCROLL,PPUADDR,PPUDATA;
     //$4014
     byte OAMDMA;
-    byte VRAMAddr,tempVRAMAddr,xScroll,writeToggle;
+    short VRAMAddr,tempVRAMAddr;
+    byte xScroll,writeToggle;
 
     Sprite[] primaryOAM;
     int nSprites;
     Sprite[] secondaryOAM;
+    int height;
+    int i;
 
     //rejestry do tla
     short bgBmpHigh,bgBmpLow;
@@ -38,7 +42,7 @@ public class PPU {
     boolean bgNextPaletteHigh,bgNextPaletteLow;
 
     //rejestry do sprite'ow
-    byte[] fgBmpHigh,fgBmpLow,fgXPos,fgAttr;
+    byte[] fgBmpHigh,fgBmpLow,fgXPos,fgAttr,timesUsed;
 
 
     PPU(){
@@ -49,6 +53,9 @@ public class PPU {
         fgBmpLow=new byte[SECONDARY_OAM_SIZE];
         fgXPos=new byte[SECONDARY_OAM_SIZE];
         fgAttr=new byte[SECONDARY_OAM_SIZE];
+        timesUsed=new byte[SECONDARY_OAM_SIZE];
+        PPUCTRL=PPUMASK=PPUSTATUS=0;
+        PPUMASK=0x1E;
     }
     class MemoryMapper {
         byte[] VRAM;
@@ -66,8 +73,12 @@ public class PPU {
         int indexFromShort(short index) {
             int index2=index < 0?index+65536:index;
             index2%=0x4000;
-            if(index2 > 0x3000)
+            if(index2 >= 0x3F20)
+                index2=0x3F00+(index2&0x1F);
+            if(index2 >= 0x3F00)
                 index2-=0x0F00;
+            else if(index2 >= 0x3000)
+                index2-=0x1000;
             return index2;
         }
     }
@@ -76,13 +87,14 @@ public class PPU {
         public byte tileIndex;
         public byte attributes;
         public byte x;
-        static final byte WIDTH=8;
-        static final byte HEIGHT=8;
+        public static final byte WIDTH=8;
         public boolean isInLine(int nr) {
-            return y<nr && y+HEIGHT>=nr;
+            int properY=y<0?y+256:y;
+            return properY<nr && properY+height>=nr;
         }
         public boolean isInColumn(int nr) {
-            return x<=nr &&x+WIDTH<nr;
+            int properX=x<0?x+256:x;
+            return properX<=nr &&properX+WIDTH<nr;
         }
         public Sprite(byte y, byte tileIndex, byte attributes, byte x) {
             this.y = y;
@@ -97,38 +109,54 @@ public class PPU {
             this.x= (byte) 0xFF;
         }
     }
-    private byte[][] render() throws InterruptedException {
+    public byte[][] render() throws InterruptedException {
         byte[][] img;
         img=new byte[FRAME_HEIGHT][];
         //linia -1 do zrobienia (nic ciekawego sie w niej nie dzieje...
+        VRAMAddr= (byte) (xScroll/8 + ((PPUCTRL&0x3)<<10)-1);
+        tempVRAMAddr= VRAMAddr;
         loadBgRegisters();
         loadBgPaletteReg();
-        updateBgRegisters();
-        for(int i=0;i<FRAME_HEIGHT;++i) {
+        bgBmpLow>>=8;
+        bgBmpHigh>>=8;
+        loadBgRegisters();
+        --VRAMAddr;
+        //updateBgRegisters();
+        for(i=0;i<FRAME_HEIGHT;++i) {
             //rownolegle trzeba wyznaczyc sprite'y do nastepnej linii - spritesInLine
             Thread t = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     //nie mozna uzyc i - trzeba cos wymyslic...
-                    spritesInLine(i);
+                    spritesInLine((byte) (i+1));
                 }
             });
             t.start();
             img[i] = drawLine();
             t.join();
             //automagicznie sie podniesie y bo x==32
-            ++VRAMAddr;
+            //++VRAMAddr;
             //trzeba zaktualizowac przesuniecie wewnatrz plytki
-            VRAMAddr+=0x2000;
+            VRAMAddr+=0x1000;
+            if((VRAMAddr&0x8000) == 0)
+                VRAMAddr-=0x0020;
+            //VRAMAddr jest 15-bitowy!
+            VRAMAddr&=0x7FFF;
             //ladujemy sprite'y dla nastepnej linii do rejestrow
             loadSprites();
             //ladujemy 2 plytki do nastepnej linii!
             loadBgRegisters();
             loadBgPaletteReg();
-            updateBgRegisters();
+            bgBmpHigh>>=8;
+            bgBmpLow>>=8;
+            loadBgRegisters();
+            --VRAMAddr;
             //??? rozkmin to Kuba
             //Two bytes are fetched, but the purpose for this is unknown. These fetches are 2 PPU cycles each.
         }
+        if((PPUMASK & 0x01) == 1)
+            //przekonwertowac na skale szarosci
+                ;
         //teraz czekamy jedna linie dla picu
         //ustawic tu VBLANK i czekac (CPU moze edytowac pamiec)
         PPUSTATUS|=0x80;
@@ -139,11 +167,11 @@ public class PPU {
     }
     private byte getBgPixel() {
         //offset = 1<<(fineX % 8)
-        int offset=1<<(xScroll%8));
+        int offset=(1<<(xScroll%8));
         if((PPUMASK &0x08) == 0)
             return 0;
-        byte res = (byte) ((bgPaletteHigh&offset)<<3+(bgPaletteLow&offset)<<2
-                +(bgBmpHigh&offset)<<1+(bgBmpLow&offset));
+        byte res = (byte) (((bgPaletteHigh&offset)<<3)+((bgPaletteLow&offset)<<2)
+                +((bgBmpHigh&offset)<<1)+((bgBmpLow&offset)));
         return res;
     }
     private void updateBgRegisters() {
@@ -152,32 +180,34 @@ public class PPU {
         bgBmpHigh>>=1;
         bgBmpLow>>=1;
         if(bgNextPaletteHigh)
-            bgBmpHigh|=0x80;
+            bgPaletteHigh|=0x80;
         else
-            bgBmpHigh&=0x70;
+            bgPaletteHigh&=0x70;
         if(bgNextPaletteLow)
-            bgBmpLow|=0x80;
+            bgPaletteLow|=0x80;
         else
-            bgBmpLow&=0x70;
+            bgPaletteLow&=0x70;
     }
     private void loadBgRegisters() {
-        bgBmpHigh&=0x00FF;
-        bgBmpLow&=0x00FF;
         ++VRAMAddr;
         //przesuniecie y wewnatrz plytki
-        int yOffset=VRAMAddr&0xE000;
-        int n=VRAMAddr&0x1800;
-        short addr= (short) (n*0x400+(VRAMAddr&0x7FF)+yOffset);
+        int yOffset=(VRAMAddr&0x7000)>>12;
+        int n=(VRAMAddr&0x0C00)>>10;
+        short addr= (short) (0x2000+n*0x400+(VRAMAddr&0x3FF)+yOffset);
+        int patternTable=0;
+        if((PPUCTRL & 0x10) == 1)
+            patternTable=0x1000;
         bgBmpHigh&=0x00FF;
-        bgBmpHigh|=VRAM.readByte((short) (0x0000+VRAM.readByte(addr)+yOffset))<<8;
+        bgBmpHigh|=VRAM.readByte((short) (patternTable+VRAM.readByte(addr)*16+yOffset))<<8;
         bgBmpLow&=0x00FF;
-        bgBmpLow|=VRAM.readByte((short) (0x0000+VRAM.readByte((short) (addr+8))+yOffset))<<8;
+        bgBmpLow|=VRAM.readByte((short) (patternTable+VRAM.readByte((short) (addr+8))*16+yOffset))<<8;
     }
     private void loadBgPaletteReg() {
         //y jest przesuniete!!!
         int y=VRAMAddr&0x3E0;
         int x=VRAMAddr&0x1F;
-        byte bgNextPalette=VRAM.readByte((short) (y/4+x/4+0x300+0x400*n));
+        int n=(VRAMAddr&0xC00)>>10;
+        byte bgNextPalette=VRAM.readByte((short) (y/4+x/4+0x23C0+0x400*n));
         int xMask=(x&0x10) > 0?1:1<<3;
         int yMask=(y&0x10) > 0?1:1<<5;
         bgNextPaletteHigh=((bgNextPalette&(2*xMask*yMask)) == 1);
@@ -186,29 +216,50 @@ public class PPU {
     private byte getPixel() {
         byte fgColor;
         byte bgColor=getBgPixel();
-        if((PPUMASK &0x10) != 0)
+        //nie rysujemy tla
+        if((PPUMASK & 0x08) == 0)
+            bgColor=0;
+        if((PPUMASK &0x10) != 0) {
             //rysujemy sprite'y (jak jest 0 to nie rysujemy)
-            for(int i=0;i<8;++i)
-                if(fgXPos[i] == 0) {
-                    fgColor = (byte) ((fgAttr[i]&0x3)<<2+(fgBmpHigh[i] & 1) << 1 + (fgBmpLow[i] & 1));
-                    fgBmpHigh[i]>>=1;
-                    fgBmpLow[i]>>=1;
-                    if(fgColor != 0) {
-                        if(i == 0 && (bgColor &0x03)!=0)
+            for(int i=0;i<8;++i) {
+                if (fgXPos[i] == 0)
+                    timesUsed[i]++;
+                else
+                    fgXPos[i]--;
+            }
+            for (int i = 0; i < 8; ++i) {
+                if (timesUsed[i] > 0 && timesUsed[i] <= 8) {
+                    fgColor = (byte) (((fgAttr[i] & 0x3) << 2) + ((fgBmpHigh[i] & 1) << 1) + (fgBmpLow[i] & 1));
+                    fgBmpHigh[i] >>= 1;
+                    fgBmpLow[i] >>= 1;
+                    if (fgColor != 0) {
+                        if (i == 0 && (bgColor & 0x03) != 0)
                             //sprite zero hit
-                            PPUSTATUS|=0x40;
-                        if((fgAttr[i]&0x10) ==0)
-                            return (byte) fgColor;
+                            PPUSTATUS |= 0x40;
+                        if ((fgAttr[i] & 0x10) == 0)
+                            return VRAM.readByte((short) (0x3F10 + fgColor));
                         else
-                            return bgColor;
+                            return VRAM.readByte((short) (0x3F00 + bgColor));
                     }
                 }
-        return bgColor;
+            }
+        }
+        return VRAM.readByte((short) (0x3F00+bgColor));
     }
     private byte[] drawLine() {
         byte[] line=new byte[FRAME_WIDTH];
+        for(int i=0;i<SECONDARY_OAM_SIZE;++i)
+            timesUsed[i]=0;
         for(int i=0;i<FRAME_WIDTH;++i) {
-            line[i] = getPixel();
+            if(i < 8 && ((PPUMASK &0x4) == 0)) {
+                if((PPUMASK & 0x04) == 0)
+                    line[i]=getBgPixel();
+                if((PPUMASK & 0x02) == 0)
+                    //nie rysuj tla...
+                ;
+            }
+            else
+                line[i] = getPixel();
             if(i%8 == 0 && i != 0)
                 loadBgRegisters();
             if(i%16 == 0 && i != 0)
@@ -219,14 +270,17 @@ public class PPU {
     }
     private void loadSprites() {
         for(int i=0;i<8;++i) {
-            if(secondaryOAM[i].tileIndex == 0xFF) {
+            if(secondaryOAM[i].tileIndex == (byte)0xFF) {
                 //powinny byc odczyty, ale moze je sobie darujemy? I tak trzeba podmienic na 0...
                 fgBmpHigh[i]=0;
                 fgBmpLow[i]=0;
             }
             else {
-                fgBmpHigh[i] = VRAM.readByte((short) (secondaryOAM[i].tileIndex + (VRAMAddr & (0xE000) >> 13)));
-                fgBmpLow[i] = VRAM.readByte((short) (secondaryOAM[i].tileIndex + (VRAMAddr & (0xE000) >> 13) + 8));
+                int patternTable=0;
+                if((PPUCTRL & 0x08) == 1)
+                    patternTable=0x1000;
+                fgBmpHigh[i] = VRAM.readByte((short) (secondaryOAM[i].tileIndex*16 + this.i-secondaryOAM[i].y+patternTable));
+                fgBmpLow[i] = VRAM.readByte((short) (secondaryOAM[i].tileIndex*16 + this.i-secondaryOAM[i].y + 8 + patternTable));
             }
             fgAttr[i] = secondaryOAM[i].attributes;
             fgXPos[i]=secondaryOAM[i].x;
@@ -234,10 +288,15 @@ public class PPU {
     }
     public void spritesInLine(byte nr) {
         int nSprInLn=0;
+        int realNr=nr<0?nr+256:nr;
+        if((PPUCTRL & 0x20) != 0)
+            height = 16;
+        else
+            height=8;
         for(int i=0;i<SECONDARY_OAM_SIZE;++i)
-            secondaryOAM[i].tileIndex= (byte) 0xFF;
+            secondaryOAM[i]= new Sprite();
         for(int i=0;i<nSprites;++i)
-            if(primaryOAM[i].isInLine(i)) {
+            if(primaryOAM[i].isInLine(realNr)) {
                 secondaryOAM[nSprInLn++]= primaryOAM[i];
                 if(nSprInLn >= 8)
                     break;
@@ -246,5 +305,27 @@ public class PPU {
         //tak mialo dzialac, ale sa bugi...
         if(nSprInLn >= 8)
             PPUSTATUS|=0x20;
+    }
+    public void ZeroVRAM() {
+        VRAM.setByte((short)0x3F21,(byte)0);
+        for(int i=0;i<0x10000;++i)
+            VRAM.setByte((short)i,(byte)0);
+    }
+    public void putSprite(int pos, byte x, byte y, byte attr,byte tileNr) {
+        primaryOAM[pos]=new Sprite(y,tileNr,attr,x);
+        nSprites++;
+    }
+    BufferedImage draw() throws InterruptedException {
+        byte[][] picture=render();
+        BufferedImage bi=new BufferedImage(FRAME_WIDTH,FRAME_HEIGHT,BufferedImage.TYPE_INT_RGB);
+        for(int i=0;i<FRAME_WIDTH;++i)
+            for(int j=0;j<FRAME_HEIGHT;++j) {
+                if (picture[j][i] == 0)
+                    bi.setRGB(i, j, 0);
+                else {
+                    bi.setRGB(i, j, 0x00FFFFFF);
+                }
+            }
+        return bi;
     }
 }
